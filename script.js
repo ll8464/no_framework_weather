bbe513e17417b937fa466eac404505abca2987dd
Lee <75961894+ll8464@users.noreply.github.com>
2022-09-20 17:23:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-  apiKey: "7a3d4408ad3936a73e4a95fd6bd93bd1",
+  apiKey: "*",
   fetchWeather: function (city) {
     fetch(
       "http://api.openweathermap.org/data/2.5/weather?q=" +
